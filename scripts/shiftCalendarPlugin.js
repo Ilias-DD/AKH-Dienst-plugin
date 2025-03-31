69163864b7ff8ccea37651447be403d6f7658c9b
Ilias-DD <ilias.deligiannis@mailfence.com>
2025-03-31 16:25:53 +0200

    Add localStorage for name caching

--- scripts/shiftCalendarPlugin.js
+++ scripts/shiftCalendarPlugin.js
@@ -73,18 +73,20 @@ function createPersonSelector(shifts) {
         select.appendChild(option);
     });
 
+    select.value = localStorage.getItem('person') == null ? select.value : localStorage.getItem('person');
+
     select.addEventListener('change', function(){
         const hideButton = document.getElementById('hide-button');
         const calendar = document.getElementById('calendarToHide');
         if (window.getComputedStyle(calendar).display === "none"){
             calendar.style.display = "block";
             hideButton.textContent = `Hide ${select.value} shifts`;
-
         }
         else{
             calendar.style.display = "none";
             hideButton.textContent = `Show ${select.value} shifts`;
         }
+        localStorage.setItem('person',select.value);
     })
 
     return select;
@@ -98,6 +100,20 @@ function createControls(shifts) {
     // Create person selector
     const select = createPersonSelector(shifts);
     controlsContainer.appendChild(select);
+
+    const previousMonth = document.createElement('button');
+    previousMonth.className = 'shift-button';
+    previousMonth.id = 'previousMonth-button';
+    previousMonth.textContent = String.fromCharCode(8249); //<
+    controlsContainer.appendChild(previousMonth);
+    previousMonth.addEventListener('click', function(){
+        const { dienstMonth, dienstYear } = extractDate();
+        const urlMediUni = new URL(window.location.href);
+        urlMediUni.searchParams.set('m', dienstMonth);
+        urlMediUni.searchParams.set("person", select.value);
+        let chachePerson = select.value
+        window.location.href = urlMediUni.toString();
+    })
 
     // Create download button
     const downloadButton = document.createElement('button');
@@ -135,24 +151,12 @@ function createControls(shifts) {
     const nextMonth = document.createElement('button');
     nextMonth.className = 'shift-button';
     nextMonth.id = 'nextMonth-button';
-    nextMonth.textContent = 'Go to next month';
+    nextMonth.textContent = String.fromCharCode(8250); //>
     controlsContainer.appendChild(nextMonth);
     nextMonth.addEventListener('click', function(){
         const { dienstMonth, dienstYear } = extractDate();
         const urlMediUni = new URL(window.location.href);
         urlMediUni.searchParams.set('m', dienstMonth + 2);
-        window.location.href = urlMediUni.toString();
-    })
-
-    const previousMonth = document.createElement('button');
-    previousMonth.className = 'shift-button';
-    previousMonth.id = 'previousMonth-button';
-    previousMonth.textContent = 'Go to previous month';
-    controlsContainer.appendChild(previousMonth);
-    previousMonth.addEventListener('click', function(){
-        const { dienstMonth, dienstYear } = extractDate();
-        const urlMediUni = new URL(window.location.href);
-        urlMediUni.searchParams.set('m', dienstMonth);
         window.location.href = urlMediUni.toString();
     })
 
